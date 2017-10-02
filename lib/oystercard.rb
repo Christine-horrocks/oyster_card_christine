8fbcf417c5c432f670780e355df0edd779baa5b5
Christine <splogy2001@hotmail.com>
2017-10-02 16:13:21 +0100

    balance and half of top-up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,11 @@
 class Oystercard
-end 
+attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def top_up
+  end
+  
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
+  it 'every new card has a balance of 0' do
+    expect(subject.balance).to eq(0)
+  end
+
+  it 'oystercard can be top_up to change balance' do
+    expect(subject).to respond_to(:top_up)
+  end
 
 end
