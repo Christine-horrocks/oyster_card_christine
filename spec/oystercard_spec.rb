cf9f340decdd30f1797a66fd92a1076741f76325
Christine <splogy2001@hotmail.com>
2017-10-02 16:42:49 +0100

    Top-up working fully

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,8 +5,12 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  it 'oystercard can be top_up to change balance' do
-    expect(subject).to respond_to(:top_up)
+  it 'oystercard can be top_up' do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'changes balance when top_up' do
+    expect{ subject.top_up 2 }.to change{ subject.balance }.by 2
   end
 
 end
